bd0b42ef6684c39490437b05952d5f7cdd4d65fa
elyoye <jorge.best95@hotmail.com>
2014-11-20 09:48:54 +0100

    Update Ejercicio.java

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,7 +1,6 @@
 
 Este es el ejercicio de evalucion de git hub de jorge parrilla romero.
-<<<<<<< HEAD
-=======
+
 
 Esta es la segunda modificacion hecha desde el editor online.
->>>>>>> 0e1e2273ed58f702fed2de355efd61e9c1ae1285
+
